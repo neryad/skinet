0b55d30b453608c9499e99aa78ecd2fb69cd9a4c
neryad <dayern.gomez@hotmail.com>
2020-12-01 23:51:05 -0400

    End section 3 part 2

---
+++ Core/Interfaces/IProductsRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductsRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+
+
+    }
+}
